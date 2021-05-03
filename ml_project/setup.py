5a41cad3c804934551e1e7fdcd8fed20877aa1eb
cherninkiy <cherninkiy@mail.ru>
2021-05-03 20:15:54 +0500

    Apply cookiecutter-DS directory strusture

---
+++ ml_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML in production course practice',
+    author='Dmitry Chеrnikоv, MADE-ML-22',
+    license='MIT',
+)
